317e5ffef10260b1e9c291703722bc43e7515f45
Utkarsh Dwivedi <dutkarsh666@gmail.com>
2026-04-19 22:04:27 +0530

    Fix: CORS open for all origins

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -15,17 +15,10 @@ const feedbackRoutes = require('./routes/feedback.routes');
 
 connecToDb();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:5173', 'http://localhost:3000'];
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error('Not allowed by CORS'));
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true
+  credentials: false
 }))
  app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
